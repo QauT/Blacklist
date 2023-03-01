6c700a4640bb44bae957f97654d120dbc1037309
QauT <77263021+QauT@users.noreply.github.com>
2023-03-01 12:31:14 +0300

    فثسف
    فثسف

---
+++ config.js
@@ -0,0 +1,9 @@
+// بأختصار شديد جدا الكود كامل انا مبرمجه ..
+// Smartu ON TOP ..
+// Developers Area ON TOP ..
+// https://discord.gg/8cWWjnJngp
+module.exports = {
+  token: "", // توكن البوت ..
+  prefix: "-", // الحرف او البداية اللي قبل الامر
+  owners: ["id1"] //ايديات اللي يقدرون يحطون بلاك لست و يشيلونه 
+}
